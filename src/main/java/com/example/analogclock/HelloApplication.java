03ba61e5f42d3a7e06f73f518029d4290ea8357c
amenta7 <mjakinnikova@hotmail.com>
2022-12-25 20:47:24 +0100

    Clock object, Button and changeColor() method.

--- src/main/java/com/example/analogclock/HelloApplication.java
+++ src/main/java/com/example/analogclock/HelloApplication.java
@@ -1,22 +1,38 @@
 package com.example.analogclock;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyCombination;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+    public void start(Stage stage) {
+        Clock clock = new Clock(210, 210, 150, Color.HOTPINK);
+
+        Text pg = new Text(" PG");
+        pg.setX(170);
+        pg.setY(260);
+        pg.setFont(Font.font("Edwardian Script ITC", 35));
+
+        Button button = new Button();
+        button.setText("Click me :D");
+        button.setTranslateX(50);
+        button.setTranslateY(50);
+        button.setOnAction(actionEvent -> clock.changeColor());
+
+        Group root = new Group();
+        root.getChildren().add(clock);
+        root.getChildren().add(pg);
+        root.getChildren().add(button);
         stage.setTitle("PG Analogue Clock");
+        Scene scene = new Scene(root, 420, 420, Color.DARKSLATEBLUE);
         stage.setScene(scene);
-        stage.setFullScreen(true);
-        stage.setFullScreenExitKeyCombination(KeyCombination.keyCombination());
+        stage.setFullScreen(false);
         stage.show();
     }
 
